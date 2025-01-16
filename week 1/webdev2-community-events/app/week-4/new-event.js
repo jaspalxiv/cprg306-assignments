cbf44338ccdff771fd439201b5e9b9156b02fa44
jaspalxiv@gmail.com <jaspalxiv@gmail.com>
2025-01-16 08:29:36 -0700

    Added week 1 content

---
+++ week 1/webdev2-community-events/app/week-4/new-event.js
@@ -0,0 +1,10 @@
+export default function NewEvent() {
+  return (
+    <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+      <h1 className="text-2xl text-gray-800 font-bold mb-8">
+        Create New Event
+      </h1>
+      <p className="text-black">Event form goes here.</p>
+    </div>
+  );
+}
--- week-2/app/page.js
+++ week-2/app/page.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 //import layout from "./layout.js"
 import Link from "next/link";
 
+
 export default function Home() {
+  let a = 5;
+  let b = 10;
   return (
     <div>
       <h1>My app week-2</h1>
