57c3c8bb42c6cab81e09890f57bfdca4517b1548
jaspalxiv@gmail.com <jaspalxiv@gmail.com>
2025-01-16 09:12:52 -0700

    A2 done

---
+++ week-2/app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link  from "next/link";
+export default function StudentInfo(){
+    return (
+        <div>
+            <h1>Jaspal Singh</h1>
+            <Link href={"https://github.com/jaspalxiv"}>My github https://github.com/jaspalxiv</Link>
+        </div>
+    );
+
+}
--- week-2/app/week_2/title.js
+++
@@ -1,8 +0,0 @@
-export default function Title(){
-    return (
-        <div className="title">
-            <h1>My Title, testing vercel update</h1>
-        </div>
-    );
-
-}
