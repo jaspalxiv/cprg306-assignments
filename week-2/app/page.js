dfde48e771bf192194809f1654a09385b26a1555
jaspalxiv@gmail.com <jaspalxiv@gmail.com>
2025-01-16 08:20:19 -0700

    initial commit

---
+++ week-2/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+//import layout from "./layout.js"
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>My app week-2</h1>
+      <p><Link href="/week_2" > Go to Week 2</Link></p>
+    </div>
+  );
+}
+
+
